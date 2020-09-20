c001a90a8f250d7d38719d96f211254b86708b72
Daniel dos Santos Filho <cin_dansantos@uolinc.com>
2020-09-20 10:28:55 -0300

    initial commit

---
+++ src/main/java/space/ifel/gamemasterapi/GameMasterApiApplication.java
@@ -0,0 +1,13 @@
+package space.ifel.gamemasterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameMasterApiApplication.class, args);
+	}
+
+}
